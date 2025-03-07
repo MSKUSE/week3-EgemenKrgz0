0867fc2d70f45b40236d7f3a3720c5a60562526d
EgemenKrgz0 <mehmetegemenkaragoz@posta.mu.edu.tr>
2025-03-07 11:59:21 +0300

    week3

--- week3/src/FindGrade.java
+++ week3/src/FindGrade.java
@@ -1,22 +1,29 @@
-public class FindGrade{
+public class FindGrade {
+public static void main(String[] args) {
+        if (args.length == 1) {
+            int score = Integer.parseInt(args[0]);
+            //Get the score from the user as a file parameter
+            //Calculate the grade based on the score
+            //Print the grade to the console
+            // 90 - 100 -> A
+            // 80 - 89 -> B
+            // 70 - 79 -> C
+            // 60 - 69 -> D
+            // 0 - 59 -> F
 
-    public static void main(String[] args){
-
-        //Get the score from the user as a file parameter
-        //Calculate the grade based on the score
-        //Print the grade to the console
-        // 90 - 100 -> A
-        // 80 - 89 -> B
-        // 70 - 79 -> C
-        // 60 - 69 -> D
-        // 0 - 59 -> F
-
-
-
-
-
+            if (score >= 90 && score <= 100) {
+                System.out.println("A");
+            } else if (score < 90 && score >= 80) {
+                System.out.println("B");
+            } else if (score < 80 && score >= 79) {
+                System.out.println("C");
+            } else if (score < 69 && score >= 60) {
+                System.out.println("D");
+            } else {
+                System.out.println("F");
+            }
+        } else{
+            System.out.println("Provide a suitable grade!");
+        }
     }
-
-
-
 }
---
+++ week3/src/FindMinimum.java
@@ -0,0 +1,25 @@
+public class FindMinimum {
+    public static void main(String[] args) {
+
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        int num3 = Integer.parseInt(args[2]);
+
+
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("num1 is grader.");
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println("num2 is grader");
+        } else {
+            System.out.println("num3 grader");
+        }
+        boolean num1_min = num1<num2 && num1 <num3;
+        boolean num2_min = num2<num1 && num2 <num3;
+
+        int min = num1_min ? num1: num2_min ? num2:num3;
+
+
+    }
+}
+
+
